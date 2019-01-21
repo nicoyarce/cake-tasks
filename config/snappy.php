d6966f2a4e654e9ffe478c925ac7bfd67325323a
Nicolas Oyarce <nicoyarce@gmail.com>
2019-01-21 16:08:52 -0300

    Cambio en snappy config

--- config/snappy.php
+++ config/snappy.php
@@ -5,7 +5,7 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary'  => 'wkhtmltopdf',
+        'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
@@ -13,7 +13,7 @@ return array(
     ),
     'image' => array(
         'enabled' => true,
-        'binary'  => 'wkhtmltopdf',
+        'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
