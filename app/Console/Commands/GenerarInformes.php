4d2dafec74734449bcea349833281524a26c5671
Nicolas Oyarce <nicoyarce@gmail.com>
2020-04-27 20:31:17 -0400

    Corrige error de comando de generacion informes

--- app/Console/Commands/GenerarInformes.php
+++ app/Console/Commands/GenerarInformes.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Log;
 use Barryvdh\Snappy\Snappy as PDF;
 use Illuminate\Support\Facades\DB;
+use App\PropiedadesGrafico;
 
 class GenerarInformes extends Command
 {
@@ -46,30 +47,32 @@ class GenerarInformes extends Command
     {
         Log::info('Inicio de generacion de informes');
         DB::beginTransaction();
-        try{
+        try {
             $proyectos = Proyecto::all();
+            $arrayConfiguraciones = PropiedadesGrafico::all();
             foreach ($proyectos as $proyecto) {
                 $tareas = $proyecto->tareas()->get();
-                $tareasJSON = $tareas->sortBy(function($tarea) {
+                $tareasJSON = $tareas->sortBy(function ($tarea) {
                                 return [$tarea->fecha_inicio, $tarea->fecha_termino];
-                            })->values()->all();
-                $tareasJSON = json_encode($tareasJSON);            
-                $pdf = \PDF::loadView('pdf', compact('proyecto', 'tareas', 'tareasJSON'));
+                })->values()->all();
+                $tareasJSON = json_encode($tareasJSON);
+                $pdf = \PDF::loadView('pdf', compact('proyecto', 'tareas', 'tareasJSON', 'arrayConfiguraciones'));
                 $pdf->setOption('encoding', 'UTF-8');
                 $pdf->setOption('javascript-delay', 1000);
                 $informe = new Informe;
-                $informe->fecha = Date::now();        
+                $informe->fecha = Date::now();
                 $informe->ruta = 'public/'.$proyecto->nombre.' - '.$informe->fecha->format('d-M-Y').'-'.$informe->fecha->format('H.i.s').'.pdf';
+                $arrayColores = PropiedadesGrafico::all()->whereNotIn('id', 6)->pluck('color');
+                $informe->colores = json_encode($arrayColores, JSON_FORCE_OBJECT);
                 $informe->proyecto()->associate($proyecto);
                 $informe->save();
                 Storage::disk('local')->put($informe->ruta, $pdf->output());
-
             }
             DB::commit();
-        } catch(\Exception $e){
+        } catch (\Exception $e) {
             Log::error('Ha ocurrido una excepcion: '.$e);
-            DB::rollback();          
+            DB::rollback();
         }
-        Log::info('Termino de generacion de informes');        
+        Log::info('Termino de generacion de informes');
     }
 }
