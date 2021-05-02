b1f5560bf953da9bdc5cc3dee018cc78ef2d8c15
Nicoyarce <nicoyarce@gmail.com>
2021-05-02 14:17:12 -0400

    Cambios en seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {        
-        $this->call(RoleTableSeeder::class);
-        DB::unprepared("
-        INSERT INTO areas (id, nombrearea, created_at, updated_at) VALUES
+    {	
+		$base = App::environment('DB_DATABASE');
+		$sql = "
+		INSERT INTO $base..areas (id, nombrearea, created_at, updated_at) VALUES
 		(1, 'Ingeniería', now(), now()),
 		(2, 'Mecánica', now(), now()),
 		(3, 'Telecomunicaciones', now(), now()),
@@ -23,10 +23,10 @@ class DatabaseSeeder extends Seeder
 		(5, 'Electrónica', now(), now()),
 		(6, 'Otra', now(), now());
 
-		INSERT INTO tipo_tareas (id, descripcion, created_at, updated_at) VALUES
+		INSERT INTO $base..tipo_tareas (id, descripcion, created_at, updated_at) VALUES
 			(1, 'Tarea', now(), now());
 
-		INSERT INTO nomenclaturasavance (id, porcentaje, glosa, created_at, updated_at, tipo_tarea) VALUES
+		INSERT INTO $base..nomenclaturasavance (id, porcentaje, glosa, created_at, updated_at, tipo_tarea) VALUES
 			(1, 0, 'Paso 1', now(), now(), 1),
 			(2, 5, 'Paso 2', now(), now(), 1),
 			(3, 10, 'Paso 3', now(), now(), 1),
@@ -44,14 +44,16 @@ class DatabaseSeeder extends Seeder
 			(15, 95, 'Paso 15', now(), now(), 1),
 			(16, 100, 'Paso 16', now(), now(), 1);
 
-		INSERT INTO propiedades_grafico (id, nombre, avance, color) VALUES
+		INSERT INTO $base..propiedades_grafico (id, nombre, avance, color) VALUES
 			(1, 'A tiempo', 0, '#28a745'),
 			(2, 'Advertencia', 60, '#ffff00'),
 			(3, 'Peligro', 90, '#f48024'),
 			(4, 'Atrasado', 100, '#dc3545'),
 			(5, 'Avance', -1, '#074590'),
 			(6, 'Porcentaje para verde', 101, '#28a745');
-        "); 
+		";
+        $this->call(RoleTableSeeder::class);
+        DB::unprepared($sql); 
         $this->call(ProyectosTareasSeeder::class);             
     }      
 }
