c0fd0a93dc91ff79df09b268fe671c0222dfe990
Nicolas Oyarce <nicoyarce@gmail.com>
2023-01-08 19:12:20 -0300

    Correccion en seeder por problema de tabla

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,51 +14,41 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $sql = "
-		INSERT INTO areas (id, nombrearea, created_at, updated_at) VALUES
-		(1, 'Ingeniería', now(), now()),
-		(2, 'Mecánica', now(), now()),
-		(3, 'Telecomunicaciones', now(), now()),
-		(4, 'Electricidad', now(), now()),
-		(5, 'Electrónica', now(), now()),
-		(6, 'Otra', now(), now());
+		DB::table('areas')->insert([
+			['id'=> 1, 'nombrearea' => 'Ingeniería'],
+			['id'=> 2, 'nombrearea' => 'Mecánica'],
+			['id'=> 3, 'nombrearea' => 'Telecomunicaciones'],
+			['id'=> 4, 'nombrearea' => 'Electricidad'],
+			['id'=> 5, 'nombrearea' => 'Electrónica'],
+			['id'=> 6, 'nombrearea' => 'Otra'],
+		]);
+		DB::table('tipo_tareas')->insert([
+			['id'=> 1, 'descripcion' => 'Tarea Normal']	,		
+			['id'=> 2, 'descripcion' => 'Tarea Extendida']	
+		]);
+		DB::table('nomenclaturasavance')->insert([
+			['id'=> 1, 'porcentaje' => 5, 'glosa' => 'Paso 1: Iniciada', 'tipo_tarea' => 1],
+			['id'=> 2, 'porcentaje' => 25, 'glosa' => 'Paso 2: Planificada', 'tipo_tarea' => 1],
+			['id'=> 3, 'porcentaje' => 50, 'glosa' => 'Paso 3: Desarrollo', 'tipo_tarea' => 1],
+			['id'=> 4, 'porcentaje' => 75, 'glosa' => 'Paso 4: Finalizando', 'tipo_tarea' => 1],
+			['id'=> 5, 'porcentaje' => 95, 'glosa' => 'Paso 5: Revision Final', 'tipo_tarea' => 1],
+			['id'=> 6, 'porcentaje' => 100, 'glosa' => 'Paso 6: Terminada', 'tipo_tarea' => 1],
+		]);
 
-		INSERT INTO tipo_tareas (id, descripcion, created_at, updated_at) VALUES
-			(1, 'Tarea', now(), now());
+		DB::table('propiedades_grafico')->insert([
+			['id'=> 1, 'nombre' => 'A Tiempo', 'avance' => 0, 'color' => '#28a745'],
+			['id'=> 2, 'nombre' => 'Advertencia', 'avance' => 60, 'color' => '#ffff00'],
+			['id'=> 3, 'nombre' => 'Peligro', 'avance' => 90, 'color' => '#f48024'],
+			['id'=> 4, 'nombre' => 'Atrasado', 'avance' => 100, 'color' => '#dc3545'],
+			['id'=> 5, 'nombre' => 'Avance', 'avance' => -1, 'color' => '#074590'],
+			['id'=> 6, 'nombre' => 'Porcentaje para verde', 'avance' => 101, 'color' => '#28a745'],
+		]);
 
-		INSERT INTO nomenclaturasavance (id, porcentaje, glosa, created_at, updated_at, tipo_tarea) VALUES
-			(1, 0, 'Paso 1', now(), now(), 1),
-			(2, 5, 'Paso 2', now(), now(), 1),
-			(3, 10, 'Paso 3', now(), now(), 1),
-			(4, 15, 'Paso 4', now(), now(), 1),
-			(5, 25, 'Paso 5', now(), now(), 1),
-			(6, 30, 'Paso 6', now(), now(), 1),
-			(7, 35, 'Paso 7', now(), now(), 1),
-			(8, 40, 'Paso 8', now(), now(), 1),
-			(9, 50, 'Paso 9', now(), now(), 1),
-			(10, 60, 'Paso 10', now(), now(), 1),
-			(11, 70, 'Paso 11', now(), now(), 1),
-			(12, 75, 'Paso 12', now(), now(), 1),
-			(13, 80, 'Paso 13', now(), now(), 1),
-			(14, 85, 'Paso 14', now(), now(), 1),
-			(15, 95, 'Paso 15', now(), now(), 1),
-			(16, 100, 'Paso 16', now(), now(), 1);
-
-		INSERT INTO propiedades_grafico (id, nombre, avance, color) VALUES
-			(1, 'A tiempo', 0, '#28a745'),
-			(2, 'Advertencia', 60, '#ffff00'),
-			(3, 'Peligro', 90, '#f48024'),
-			(4, 'Atrasado', 100, '#dc3545'),
-			(5, 'Avance', -1, '#074590'),
-			(6, 'Porcentaje para verde', 101, '#28a745');		
-
-		INSERT INTO categoria (id, nombre) VALUES
-			(1, 'PID'),
-			(2, 'MOD'),
-			(3, 'REC');
-		";
+		DB::table('categoria')->insert([
+			['id'=> 1, 'nombre' => 'Actividad Fisica'],
+			['id'=> 2, 'nombre' => 'Actividad Mental'],
+		]);
         $this->call(RoleTableSeeder::class);
-        DB::unprepared($sql);
         $this->call(ProyectosTareasSeeder::class);
     }
 }
